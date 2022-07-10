47389861eae8764da959cc9adce4fd738cf61468
leejihoon0312 <82079111+leejihoon0312@users.noreply.github.com>
2022-07-10 12:44:36 +0900

    feat: SecurityAutoConfiguration 일시중지

--- src/main/java/com/deadline826/bedi/BediApplication.java
+++ src/main/java/com/deadline826/bedi/BediApplication.java
@@ -2,8 +2,9 @@ package com.deadline826.bedi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BediApplication {
 
 	public static void main(String[] args) {
