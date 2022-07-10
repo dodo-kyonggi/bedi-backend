8412c1430a919f4903cf359b1339d7263340e90e
leejihoon0312 <82079111+leejihoon0312@users.noreply.github.com>
2022-07-10 09:56:38 +0900

    feat: 초기셋팅

---
+++ src/main/java/com/deadline826/bedi/BediApplication.java
@@ -0,0 +1,13 @@
+package com.deadline826.bedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BediApplication.class, args);
+	}
+
+}
