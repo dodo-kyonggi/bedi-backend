7ee1ba031fc731210a97cec8fc69503ec29568f3
jihoon <wlgnsdl1016@naver.com>
2022-08-15 08:33:44 +0900

    레디스에 토큰저장

--- src/main/java/com/deadline826/bedi/security/JwtConstants.java
+++ src/main/java/com/deadline826/bedi/security/JwtConstants.java
@@ -9,8 +9,8 @@ public class JwtConstants {
     public static final long DAY = 24 * HOUR;
     public static final long MONTH = 30 * DAY;
 
-    public static final long AT_EXP_TIME =  1 * MINUTE;
-    public static final long RT_EXP_TIME =  2 * MINUTE;
+    public static final long AT_EXP_TIME =  10 * MINUTE;
+    public static final long RT_EXP_TIME =  20 * MINUTE;
 
     // Secret
     public static final String JWT_SECRET = "kakao_test_key";
